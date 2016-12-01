206e8e8c36c6e38d595b9501028d13229ddfb2d2
Oliver <oliveromondi65@gmail.com>
2016-12-01 09:24:52 +0300

    Taking care of the occurences between 1 and the number passed

--- prime_number_solution.py
+++ prime_number_solution.py
@@ -1,6 +1,6 @@
 #function that tests for prime numbers
 def prime_number_solution(number): #function takes in number as an arguement
-	for x in range(number):
+	for x in range(number): #taking care of the occurances
     		if number % x == 0: #testing for prime numbers
         		return False #return false if not prime number
     	return True #return true if the funtion is a prime number
