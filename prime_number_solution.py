7b4e08005a0be3ccb75c863d88d7e6920c028eac
Oliver <oliveromondi65@gmail.com>
2016-12-01 09:23:04 +0300

    Returns true if the number is true

--- prime_number_solution.py
+++ prime_number_solution.py
@@ -3,6 +3,6 @@ def prime_number_solution(number): #function takes in number as an arguement
 	for x in range(number):
     		if number % x == 0: #testing for prime numbers
         		return False #return false if not prime number
-    	return True
+    	return True #return true if the funtion is a prime number
 
 
