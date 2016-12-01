13427f02b30318b359414c8352f5c1e2fb09d77c
Oliver <oliveromondi65@gmail.com>
2016-12-01 14:53:31 +0300

    Final changes

--- Fabonacci.py
+++ Fabonacci.py
@@ -1,9 +1,10 @@
 def Fabonacci(arg): 
-    if arg == 0:
-    	return 0
+    for x in range (arg):
+    	if arg == 0:
+    		return 0
     
-    elif arg == 1: 
-    	return 1
+    	elif arg == 1: 
+    		return 1
     
-    else: 
-    	return F(arg-1)+F(arg-2)
+    	else: 
+    		return F(arg-1)+F(arg-2)
