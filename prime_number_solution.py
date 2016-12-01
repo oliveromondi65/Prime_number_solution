9ea01004032e78ac82080f0a4e03b20227fa0618
Oliver <oliveromondi65@gmail.com>
2016-12-01 09:22:22 +0300

    Testing for prime numbers

--- prime_number_solution.py
+++ prime_number_solution.py
@@ -2,7 +2,7 @@
 def prime_number_solution(number): #function takes in number as an arguement
 	for x in range(number):
     		if number % x == 0: #testing for prime numbers
-        		return False
+        		return False #return false if not prime number
     	return True
 
 
